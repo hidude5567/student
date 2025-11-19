cdd61d39680be665fb2e616e55a870553886b237
hidude5567 <nolansenter@s.fwc.org>
2025-11-19 15:40:28 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function doodoo() {
   var ss = prompt("password");
   if (ss == "songsss") {
-    window.location.href = "r.html";
+    location.href = "r.html"
   }
 }
-  
---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 5000), NoCacheHTTPRequestHandler)
+    print('Server running on port 5000...')
+    server.serve_forever()
